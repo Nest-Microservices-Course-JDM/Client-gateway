9efbf7bc8c3d212bd33ad95bd74f37a693a99bd0
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2025-09-24 23:26:04 -0500

    feat: initialize NestJS client gateway with NATS transport
    - Add package.json with dependencies and scripts for NestJS application
    - Create app.module.ts to import Products, Orders, and NATS modules
    - Implement PaginationDto for paginated responses
    - Add RpcCustomExceptionFilter for handling RPC exceptions
    - Configure environment variables validation in envs.ts
    - Create NATS module for microservices communication
    - Develop Orders and Products modules with respective controllers and DTOs
    - Implement basic e2e tests for application
    - Set up TypeScript configuration for the project
    
    The dupped code is because I followed greate coding microservices practices

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,53 @@
+import { Controller, Get, Post, Body, Patch, Param, Inject, ParseUUIDPipe, Query } from '@nestjs/common';
+
+import { NATS_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { CreateOrderDto, OrderPaginationDto, StatusDto, } from './dto';
+import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { response } from 'express';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.client.send({cmd: 'createOrder'}, createOrderDto)
+  }
+
+  @Get()
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    return this.client.send({cmd: 'findAllOrders'}, orderPaginationDto)
+    .pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+        })
+      )
+  }
+
+  @Get('findByID/:id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.client.send({cmd: 'findOneOrder'},{id})
+  }
+  
+  //FIND BY STATUS
+  @Get(':status')
+  findAllByStatus(@Param() statusDto: StatusDto, @Query() paginationDto: PaginationDto) {
+    return this.client.send({cmd: 'findAllOrders'},{...paginationDto, status: statusDto.status})
+    .pipe(
+      catchError(err => {throw new RpcException(err)})
+    )
+  }
+
+  @Patch(':id')
+  changeStatus(@Param('id', ParseUUIDPipe) id: string, @Body() statusDto: StatusDto){
+    return this.client.send({cmd: 'changeOrderStatus'}, {id, status: statusDto.status})
+    .pipe(
+      catchError(err => {throw new RpcException(err)})
+    )
+    
+  }
+}
